a8a21c432e8da29af8052cb9b1b6a4a130a35728
mzuer <z_marie_7@hotmail.com>
2019-10-15 15:10:45 +0200

    15.10.19

---
+++ report_figure1.R
@@ -0,0 +1,318 @@
+# FIGURE 1: barplot # TADs, # signif. TADs, # signif. TADs and FDR
+
+options(scipen=100)
+
+setDir = ""
+
+# Rscript report_figure1.R   # 
+
+script_name <- "report_figure1.R"
+
+startTime <- Sys.time()
+
+cat("> START ", script_name, "\n")
+
+SSHFS <- FALSE
+
+require(ggplot2)
+require(ggpubr)
+require(ggforce)
+require(reshape2)
+require(foreach)
+require(doMC)
+require(plotrix)
+registerDoMC(ifelse(SSHFS, 2, 40))
+
+source("../Cancer_HiC_data_TAD_DA/utils_fct.R")
+source("../2_Yuanlong_Cancer_HiC_data_TAD_DA/utils_fct.R")
+source("../Yuanlong_Cancer_HiC_data_TAD_DA/subtype_cols.R")
+
+col1 <- get_palette("Dark2", 4)[1]
+col2 <- get_palette("Dark2", 4)[2]
+col3 <- get_palette("Dark2", 4)[3]
+col4 <- get_palette("Dark2", 4)[4]
+
+plotType <- "svg"
+myHeight <- ifelse(plotType=="png", 400, 7)
+myWidth <- ifelse(plotType=="png", 600, 10)
+plotCex <- 1.2
+
+myWidthGG <- 12
+myHeightGG <- 8
+
+outFolder <- "REPORT_FIGURE1"
+dir.create(outFolder, recursive=TRUE)
+
+signifTAD_thresh <- 0.05
+signifTAD_FDR <- 0.2
+
+final_table_file <- file.path("CREATE_FINAL_TABLE/all_result_dt.Rdata")
+stopifnot(file.exists(final_table_file))
+final_table_DT <- get(load(final_table_file))
+
+final_table_DT$pval_signif <- final_table_DT$adjPvalComb <= signifTAD_thresh
+final_table_DT$fdr_signif <- final_table_DT[, paste0("signifFDR_", signifTAD_FDR)]
+final_table_DT$pval_fdr_signif <- final_table_DT$fdr_signif & final_table_DT$pval_signif
+
+count_tot_dt <- aggregate(pval_signif ~ hicds + exprds, data=final_table_DT, FUN=length)
+colnames(count_tot_dt)[colnames(count_tot_dt) == "pval_signif"] <- "tot"
+count_tot_dt_m <- melt(count_tot_dt, id=c("hicds", "exprds"))
+count_pval_dt <- aggregate(pval_signif ~ hicds + exprds, data=final_table_DT, FUN=sum)
+count_pval_dt_m <- melt(count_pval_dt, id=c("hicds", "exprds"))
+count_fdr_dt <- aggregate(fdr_signif ~ hicds + exprds, data=final_table_DT, FUN=sum)
+count_fdr_dt_m <- melt(count_fdr_dt, id=c("hicds", "exprds"))
+count_pval_fdr_dt <- aggregate(pval_fdr_signif ~ hicds + exprds, data=final_table_DT, FUN=sum)
+count_pval_fdr_dt_m <- melt(count_pval_fdr_dt, id=c("hicds", "exprds"))
+
+count_tot_dt <- count_tot_dt[order(count_tot_dt$tot, decreasing = TRUE),]
+count_tot_dt$dataset <- paste0(count_tot_dt$hicds, "\n", count_tot_dt$exprds)
+ds_levels <- as.character(count_tot_dt$dataset)
+stopifnot(!duplicated(ds_levels))
+
+stopifnot(count_tot_dt$exprds %in% names(all_cmps))
+
+ds_cols <- all_cols[all_cmps[paste0(count_tot_dt$exprds)]]
+stopifnot(!is.na(ds_cols))
+
+
+plot_dt <- rbind(count_tot_dt_m, count_pval_dt_m, count_fdr_dt_m, count_pval_fdr_dt_m)
+plot_dt$dataset <-paste0(plot_dt$hicds, "\n", plot_dt$exprds)
+plot_dt$dataset <- factor(plot_dt$dataset, levels = ds_levels)
+stopifnot(!is.na(plot_dt$dataset))
+
+
+colsOrder <- c("tot", "pval_signif", "fdr_signif", "pval_fdr_signif")
+
+plot_dt$variable <- factor(plot_dt$variable, levels=colsOrder)
+
+nDS <- length(ds_levels)
+
+p_var <-  ggplot(plot_dt, aes(x = dataset, y = value, fill = variable)) + 
+  geom_bar(position="dodge", stat="identity") +
+  coord_cartesian(expand = FALSE) +
+  ggtitle("Total # TADs and # signif. TADs", subtitle = paste0("# datasets = ", nDS, " (p-val. thresh. = ", signifTAD_thresh, ")"))+
+  scale_x_discrete(name="")+
+  labs(fill="")+
+  scale_fill_manual(values=c(col1,col2, col3, col4))+
+  scale_y_continuous(name=paste0("# signif. TADs"),
+                     breaks = scales::pretty_breaks(n = 10))+
+  theme( # Increase size of axis lines
+    # strip.text = element_text(size = 12),
+    # top, right, bottom and left
+    # plot.margin = unit(c(1, 1, 4.5, 1), "lines"),
+    plot.title = element_text(hjust = 0.5, face = "bold", size=16),
+    plot.subtitle = element_text(hjust = 0.5, face = "italic", size = 14),
+    panel.grid = element_blank(),
+    panel.grid.major.y = element_line(colour = "grey"),
+    panel.grid.minor.y = element_line(colour = "grey"),
+    strip.text.x = element_text(size = 10),
+    axis.line.x = element_line(size = .2, color = "black"),
+    axis.line.y = element_line(size = .3, color = "black"),
+    axis.text.y = element_text(color="black", hjust=1,vjust = 0.5, size=10),
+    axis.text.x = element_text(color=ds_cols, hjust=1,vjust = 0.5, size=7, angle=90),
+    axis.ticks.x = element_blank(),
+    axis.title.y = element_text(color="black", size=12),
+    axis.title.x = element_text(color="black", size=12),
+    panel.border = element_blank(),
+    panel.background = element_rect(fill = "transparent"),
+    legend.background =  element_rect(),
+    legend.key = element_blank(),
+    legend.title = element_text(face="bold")
+  )
+
+outFile <- file.path(outFolder, paste0("nbrTADs_tot_and_signif_ggbar.", plotType))
+ggsave(plot = p_var, filename = outFile, height=myHeightGG, width = myWidthGG)
+cat(paste0("... written: ", outFile, "\n"))
+
+
+plot_dt2 <- plot_dt[plot_dt$variable != "tot",]
+ds_levels2 <- plot_dt2$dataset[order(plot_dt2$value[plot_dt2$variable=="pval_signif"])]
+plot_dt2$dataset <- factor(as.character(plot_dt2$dataset), levels=ds_levels2)
+
+
+p_var2 <-  ggplot(plot_dt2, aes(x = dataset, y = value, fill = variable)) + 
+  geom_bar(position="dodge", stat="identity") +
+  coord_cartesian(expand = FALSE) +
+  ggtitle("Total # of TADs and # signif. TADs", subtitle = paste0("# datasets = ", nDS, " (p-val. thresh. = ", signifTAD_thresh, ")"))+
+  scale_x_discrete(name="")+
+  labs(fill="")+
+  scale_fill_manual(values=c(col2, col3,col4))+
+  scale_y_continuous(name=paste0("# signif. TADs"),
+                     breaks = scales::pretty_breaks(n = 10))+
+  theme( # Increase size of axis lines
+    # strip.text = element_text(size = 12),
+    # top, right, bottom and left
+    # plot.margin = unit(c(1, 1, 4.5, 1), "lines"),
+    plot.title = element_text(hjust = 0.5, face = "bold", size=16),
+    plot.subtitle = element_text(hjust = 0.5, face = "italic", size = 14),
+    panel.grid = element_blank(),
+    panel.grid.major.y = element_line(colour = "grey"),
+    panel.grid.minor.y = element_line(colour = "grey"),
+    strip.text.x = element_text(size = 10),
+    axis.line.x = element_line(size = .2, color = "black"),
+    axis.line.y = element_line(size = .3, color = "black"),
+    axis.text.y = element_text(color="black", hjust=1,vjust = 0.5, size=10),
+    axis.text.x = element_text(color=ds_cols, hjust=1,vjust = 0.5, size=7, angle=90),
+    axis.ticks.x = element_blank(),
+    axis.title.y = element_text(color="black", size=12),
+    axis.title.x = element_text(color="black", size=12),
+    panel.border = element_blank(),
+    panel.background = element_rect(fill = "transparent"),
+    legend.background =  element_rect(),
+    legend.key = element_blank(),
+    legend.title = element_text(face="bold")
+  )
+outFile <- file.path(outFolder, paste0("nbrTADs_only_signif_ggbar.", plotType))
+ggsave(plot = p_var2, filename = outFile, height=myHeightGG, width = myWidthGG)
+cat(paste0("... written: ", outFile, "\n"))
+
+
+#################### barplot with breaks
+from_gap <- max(plot_dt$value[plot_dt$variable != "tot"]) + 10
+to_gap <- min(plot_dt$value[plot_dt$variable == "tot"]) - 10
+stopifnot(to_gap > from_gap)
+
+
+barplot_dt <- merge(count_pval_dt, merge(count_fdr_dt, merge(count_tot_dt, count_pval_fdr_dt, by=c("hicds", "exprds")), by=c("hicds", "exprds")), by=c("hicds", "exprds"))
+barplot_dt[,"hicds"] <- NULL
+barplot_dt[,"exprds"] <- NULL
+barplot_dt <- barplot_dt[order(barplot_dt$tot, decreasing = TRUE),]
+rownames(barplot_dt) <- barplot_dt$dataset
+barplot_dt$dataset <- NULL
+# barplot(t(barplot_dt), beside = TRUE)
+barplot_dt <- barplot_dt[,paste0(colsOrder)]
+
+bar_plot_dt <- t(barplot_dt)
+
+nGaps <- ncol(bar_plot_dt) * (nrow(bar_plot_dt)+1) - 1
+
+# Hack for grouping (leaves the extra space at the end)
+bar_plot_dt_withGroups <- as.vector(rbind(bar_plot_dt, rep(NA, ncol(bar_plot_dt))))[1:nGaps]
+
+barcols <- all_cols[all_cmps[paste0(gsub(".+\n(.+)", "\\1", colnames(bar_plot_dt)))]]
+stopifnot(!is.na(barcols))
+
+outFile <- file.path(outFolder, paste0("nbrTADs_tot_and_signif_breakbar.", plotType))
+do.call(plotType, list(outFile, height=myHeight, width=myWidth))
+par(bty="n")
+a = gap.barplot(ceiling(as.matrix(bar_plot_dt_withGroups)), 
+                gap=c(from_gap,to_gap),
+                xlab="",
+                ylab="# of TADs",
+                main = "Total # of TADs and # signif. TADs",
+                col=rep(c(col1,col2,col3,col4,1), ncol(bar_plot_dt)),  # add 1 for the gap
+                ytics = pretty(as.numeric(bar_plot_dt), n=20),
+                xaxt='n') # disable the default x-axis
+
+x_pos <- seq(2.5, by=5, length.out = ncol(bar_plot_dt))
+# add axis labels at mean position
+# axis(1, at=x_pos, colnames(bar_plot_dt), las=2, cex.axis=0.5, line=-1, lwd=0, lwd.ticks=1)
+axis(1, at=x_pos, las=2, cex.axis=0.5, line=-1, lwd=0, lwd.ticks=1, labels=F)
+mtext(side=1, at=x_pos, colnames(bar_plot_dt), las=2, col = barcols, cex = 0.4, line=-0.5)
+
+legend("topright", legend = colsOrder,
+       bty="n",  
+       fill=c(col1,col2,col3,col4)) 
+axis.break(2, from_gap, breakcol="snow", style="gap")
+axis.break(2, from_gap*(1+0.02), breakcol="black", style="slash")
+foo <- dev.off()
+cat(paste0("... written: ", outFile, "\n"))
+
+
+bar_plot_dt_noTot <- bar_plot_dt[rownames(bar_plot_dt) != "tot", ,drop=F]
+bar_plot_dt_noTot <- bar_plot_dt_noTot[,order(bar_plot_dt_noTot["pval_signif",], decreasing = TRUE)]
+nGaps_noTot <- ncol(bar_plot_dt_noTot) * (nrow(bar_plot_dt_noTot)+1) - 1
+bar_plot_dt_withGroups_noTot <- as.vector(rbind(bar_plot_dt_noTot, rep(NA, ncol(bar_plot_dt_noTot))))[1:nGaps_noTot]
+
+outFile <- file.path(outFolder, paste0("nbrTADs_tot_and_signif_breakbar.", plotType))
+do.call(plotType, list(outFile, height=myHeight, width=myWidth))
+par(bty="n")
+# barplot(ceiling(as.matrix(bar_plot_dt_withGroups_noTot)), 
+#                 beside = TRUE,
+#                 xlab="",
+#                 ylab="# of TADs",
+#                 main = "Total # of TADs and # signif. TADs",
+#                 col=rep(c(col2,col3,col4,1), ncol(bar_plot_dt_noTot)),  # add 1 for the gap
+#                 # ytics = pretty(as.numeric(bar_plot_dt), n=20),
+#                 xaxt='n') # disable the default x-axis
+from_gap <- max(bar_plot_dt_noTot[rownames(bar_plot_dt_noTot) != "pval_signif", ]) + 2
+to_gap <- min(bar_plot_dt_noTot[rownames(bar_plot_dt_noTot) == "pval_signif", ]) - 2
+stopifnot(to_gap > from_gap)
+
+a = gap.barplot(ceiling(as.matrix(bar_plot_dt_withGroups_noTot)), 
+                gap=c(from_gap,to_gap),
+                xlab="",
+                ylab="# of TADs",
+                main = "Total # of TADs and # signif. TADs",
+                col=rep(c(col2,col3,col4,1), ncol(bar_plot_dt_noTot)),  # add 1 for the gap
+                ytics = pretty(as.numeric(bar_plot_dt_noTot), n=20),
+                xaxt='n') # disable the default x-axis
+
+x_pos <- seq(2.5, by=5, length.out = ncol(bar_plot_dt))
+# add axis labels at mean position
+# axis(1, at=x_pos, colnames(bar_plot_dt), las=2, cex.axis=0.5, line=-1, lwd=0, lwd.ticks=1)
+axis(1, at=x_pos, las=2, cex.axis=0.5, line=-1, lwd=0, lwd.ticks=1, labels=F)
+mtext(side=1, at=x_pos, colnames(bar_plot_dt), las=2, col = barcols, cex = 0.4, line=-0.5)
+
+legend("topright", legend = colsOrder[colsOrder!="tot"],
+       bty="n",  
+       fill=c(col2,col3,col4)) 
+axis.break(2, from_gap, breakcol="snow", style="gap")
+axis.break(2, from_gap*(1+0.02), breakcol="black", style="slash")
+foo <- dev.off()
+cat(paste0("... written: ", outFile, "\n"))
+
+
+p_var <-  ggplot(plot_dt, aes(x = dataset, y = value, fill = variable)) + 
+  geom_bar(position="dodge", stat="identity") +
+  coord_cartesian(expand = FALSE) +
+  ggtitle("Total # TADs and # signif. TADs", subtitle = paste0("# datasets = ", nDS, " (p-val. thresh. = ", signifTAD_thresh, ")"))+
+  scale_x_discrete(name="")+
+  labs(fill="")+
+  scale_fill_manual(values=c(col1,col2, col3, col4))+
+  scale_y_continuous(name=paste0("# signif. TADs"),
+                     breaks = scales::pretty_breaks(n = 10))+
+  theme( # Increase size of axis lines
+    # strip.text = element_text(size = 12),
+    # top, right, bottom and left
+    # plot.margin = unit(c(1, 1, 4.5, 1), "lines"),
+    plot.title = element_text(hjust = 0.5, face = "bold", size=16),
+    plot.subtitle = element_text(hjust = 0.5, face = "italic", size = 14),
+    panel.grid = element_blank(),
+    panel.grid.major.y = element_line(colour = "grey"),
+    panel.grid.minor.y = element_line(colour = "grey"),
+    strip.text.x = element_text(size = 10),
+    axis.line.x = element_line(size = .2, color = "black"),
+    axis.line.y = element_line(size = .3, color = "black"),
+    axis.text.y = element_text(color="black", hjust=1,vjust = 0.5, size=10),
+    axis.text.x = element_text(color=ds_cols, hjust=1,vjust = 0.5, size=7, angle=90),
+    axis.ticks.x = element_blank(),
+    axis.title.y = element_text(color="black", size=12),
+    axis.title.x = element_text(color="black", size=12),
+    panel.border = element_blank(),
+    panel.background = element_rect(fill = "transparent"),
+    legend.background =  element_rect(),
+    legend.key = element_blank(),
+    legend.title = element_text(face="bold")
+  )
+
+p_var <- p_var + facet_zoom(y=variable!="tot", zoom.data = ifelse(variable=="tot", FALSE, NA))
+
+outFile <- file.path(outFolder, paste0("nbrTADs_tot_and_zoomSignif_ggbar.", plotType))
+ggsave(plot = p_var, filename = outFile, height=myHeightGG, width = myWidthGG*1.5)
+cat(paste0("... written: ", outFile, "\n"))
+
+
+
+##########################################################################################
+##########################################################################################
+##########################################################################################
+
+cat("*** DONE - ", script_name, "\n")
+cat(paste0(startTime, "\n", Sys.time(), "\n"))
+
+
+
+
+
+
---
+++ report_figure2.R
@@ -0,0 +1,169 @@
+# FIGURE 2: scatterplot meanFC meanCorr color by pval
+
+options(scipen=100)
+
+setDir = ""
+
+# Rscript report_figure2.R   # 
+
+script_name <- "report_figure2.R"
+
+startTime <- Sys.time()
+
+cat("> START ", script_name, "\n")
+
+SSHFS <- FALSE
+
+require(ggplot2)
+require(ggpubr)
+require(ggforce)
+require(reshape2)
+require(foreach)
+require(doMC)
+require(plotrix)
+registerDoMC(ifelse(SSHFS, 2, 40))
+
+source("../Cancer_HiC_data_TAD_DA/utils_fct.R")
+source("../2_Yuanlong_Cancer_HiC_data_TAD_DA/utils_fct.R")
+source("../Yuanlong_Cancer_HiC_data_TAD_DA/subtype_cols.R")
+
+col1 <- get_palette("Dark2", 4)[1]
+col2 <- get_palette("Dark2", 4)[2]
+col3 <- get_palette("Dark2", 4)[3]
+col4 <- get_palette("Dark2", 4)[4]
+
+plotType <- "png"
+myHeight <- ifelse(plotType=="png", 400, 7)
+myWidth <- ifelse(plotType=="png", 600, 10)
+plotCex <- 1.2
+
+myWidthGG <- 12
+myHeightGG <- 12
+
+outFolder <- "REPORT_FIGURE2"
+dir.create(outFolder, recursive=TRUE)
+
+signifTAD_thresh <- 0.05
+signifTAD_FDR <- 0.2
+
+final_table_file <- file.path("CREATE_FINAL_TABLE/all_result_dt.Rdata")
+stopifnot(file.exists(final_table_file))
+final_table_DT <- get(load(final_table_file))
+
+final_table_DT$pval_signif <- final_table_DT$adjPvalComb <= signifTAD_thresh
+final_table_DT$fdr_signif <- final_table_DT[, paste0("signifFDR_", signifTAD_FDR)]
+final_table_DT$pval_fdr_signif <- final_table_DT$fdr_signif & final_table_DT$pval_signif
+
+final_table_DT$adjPvalComb_log10 <- -log10(final_table_DT$adjPvalComb)
+
+format_my_plot <- function(p) {
+  p <- p + gradient_color("YlOrRd") + 
+    scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) + 
+    scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) + 
+    theme( 
+      plot.subtitle = element_text(hjust=0.5),
+      axis.text.y = element_text(color="black", size=12),
+      axis.title.y = element_text(color="black", size=14, face="bold"),
+      axis.text.x = element_text(color="black", size=12),
+      axis.title.x = element_text(color="black", size=14, face="bold")
+    )
+  return(p)
+}
+
+format_my_plot_facet <- function(p) {
+  p <- format_my_plot(p)
+  p <- p + theme(strip.text.x = element_text(size = 14))
+  return(p)
+}
+
+
+
+
+p1 <- ggscatter(final_table_DT, 
+                subtitle=paste0("n=", nrow(final_table_DT)),
+               x = "meanLogFC", 
+               y = "meanCorr",
+               color = "adjPvalComb_log10")
+p1 <- format_my_plot(p1)
+  
+p1b <- ggscatter(final_table_DT[final_table_DT$adjPvalComb <=0.01,], 
+                 subtitle=paste0("n=", nrow(final_table_DT[final_table_DT$adjPvalComb <=0.01,])),
+               x = "meanLogFC", 
+               y = "meanCorr",
+               color = "adjPvalComb_log10")
+p1b <- format_my_plot(p1b)
+
+final_table_DT$cmpType <- all_cmps[final_table_DT$exprds]
+stopifnot(!is.na(final_table_DT$cmpType))
+
+p2 <- ggscatter(final_table_DT, 
+          x = "meanLogFC",
+          y = "meanCorr", 
+          color = "adjPvalComb_log10",
+          scales="free",
+          facet.by = "cmpType")
+p2 <- format_my_plot_facet(p2)
+
+
+for(cmp_type in unique(as.character(final_table_DT$cmpType))) {
+  p2annot_1 <-  data.frame(meanLogFC = min(final_table_DT$meanLogFC[final_table_DT$cmpType == cmp_type]),
+                           meanCorr = min(final_table_DT$meanCorr[final_table_DT$cmpType == cmp_type]),
+                           lab = paste0("n=", sum(final_table_DT$cmpType == cmp_type), 
+                           "; #DS=", length(unique(paste0(final_table_DT[final_table_DT$cmpType == cmp_type,]$hicds, final_table_DT[final_table_DT$cmpType == cmp_type,]$exprds)))),
+                           cmpType = paste0(cmp_type))
+  p2 <- p2 + geom_text(data = p2annot_1, label = p2annot_1$lab, hjust=0, vjust=0)
+}
+
+
+
+tmpdt <- final_table_DT[final_table_DT$adjPvalComb <=0.01,]
+
+p2b <- ggscatter(tmpdt, 
+                x = "meanLogFC",
+                y = "meanCorr", 
+                color = "adjPvalComb_log10",
+                scales="free",
+                facet.by = "cmpType")
+p2b <- format_my_plot_facet(p2b)
+
+for(cmp_type in unique(as.character(tmpdt$cmpType))) {
+  p2bannot_1 <-  data.frame(meanLogFC = min(tmpdt$meanLogFC[tmpdt$cmpType == cmp_type]),
+                           meanCorr = min(tmpdt$meanCorr[tmpdt$cmpType == cmp_type]),
+                           lab = paste0("n=", sum(tmpdt$cmpType == cmp_type), "; #DS=", length(unique(paste0(tmpdt[tmpdt$cmpType == cmp_type,]$hicds, tmpdt[tmpdt$cmpType == cmp_type,]$exprds)))),
+                           cmpType = paste0(cmp_type))
+  p2b <- p2b + geom_text(data = p2bannot_1, label = p2bannot_1$lab, hjust=0, vjust=0)
+}
+
+
+
+arr1 <- ggarrange(p1, p1b, ncol = 2, labels = c("ALL DATA", "ONLY SIGNIF. TADs")) #
+arr2 <- ggarrange(p2, p2b, nrow = 2, labels = c("ALL DATA", "ONLY SIGNIF. TADs")) #
+
+
+arr_all <- ggarrange(arr1, arr2,
+          nrow = 2,
+          heights=c(1/3,2/3)
+          )
+
+outFile <- file.path(outFolder, paste0("meanCorr_meanFC_allPlots.", plotType))
+ggsave(plot = arr_all, filename = outFile, height=myHeightGG*2, width = myWidthGG*2)
+cat(paste0("... written: ", outFile, "\n"))
+
+
+
+
+
+##########################################################################################
+##########################################################################################
+##########################################################################################
+
+cat("*** DONE - ", script_name, "\n")
+cat(paste0(startTime, "\n", Sys.time(), "\n"))
+
+
+
+
+
+
+
+
