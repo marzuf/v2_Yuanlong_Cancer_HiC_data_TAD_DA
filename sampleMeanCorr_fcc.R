848331c88d16a8a0b7c66994485a50ff486925ed
mzuer <z_marie_7@hotmail.com>
2020-05-03 10:26:15 +0200

    4520

---
+++ sampleMeanCorr_fcc.R
@@ -0,0 +1,101 @@
+
+# Rscript sampleMeanCorr_fcc
+
+SSHFS <- F
+setDir <- ifelse(SSHFS, "/media/electron", "")
+
+plotType <- "svg"
+myHeight <- ifelse(plotType == "png", 480 , 7)
+myWidth <- ifelse(plotType == "png", 600, 10)
+plotCex <- 1.4
+
+require(flux)
+require(foreach)
+require(doMC)
+registerDoMC(40)
+
+outFolder <- file.path("SAMPLEMEANCORR_FCC")
+dir.create(outFolder, recursive=TRUE)
+
+options(scipen=100)
+
+startTime <- Sys.time()
+
+buildData <- TRUE
+
+hicds <- "ENCSR489OCU_NCI-H460_40kb"
+exprds <- "TCGAlusc_norm_lusc"
+
+runFolder <-  "."
+pipFolder <- file.path(runFolder, "PIPELINE/OUTPUT_FOLDER")
+
+script7_name <- "7sameNbr_runPermutationsMeanTADCorr"
+script8_name <- "8cOnlyFCC_runAllDown"
+
+all_hicds <- list.files(pipFolder)
+all_exprds <- sapply(all_hicds, function(x) list.files(file.path(pipFolder, x)))
+all_obs_hicds <- all_hicds[! (grepl("RANDOM", all_hicds) | grepl("PERMUT", all_hicds))]
+all_obs_exprds <- sapply(all_obs_hicds, function(x) list.files(file.path(pipFolder, x)))
+
+
+
+# all_obs_hicds=all_obs_hicds[1]
+if(buildData){
+  
+  all_fcc_corr_dt <- foreach(hicds = all_obs_hicds, .combine='rbind') %dopar% {
+    exprds = all_exprds[[paste0(hicds)]][1]
+    hicds_dt <- foreach(exprds = all_obs_exprds[[paste0(hicds)]], .combine='rbind') %do% {
+      
+      meanCorr_samp_file <- file.path(pipFolder, hicds, exprds, script7_name, "meanCorr_sample_around_TADs_sameNbr.Rdata")
+      stopifnot(file.exists(meanCorr_samp_file))
+      meanCorr_data <- get(load(meanCorr_samp_file))
+      
+      all_meanCorr_values <- lapply(meanCorr_data, function(x) x[["meanCorr"]])
+      
+      meanCorr_dt <- data.frame(region=names(all_meanCorr_values), sample_meanCorr = as.numeric(all_meanCorr_values), stringsAsFactors = FALSE)
+      meanCorr_dt <- na.omit(meanCorr_dt)
+      
+      fcc_file <- file.path(pipFolder, hicds, exprds, script8_name, "all_obs_prodSignedRatio.Rdata")
+      stopifnot(file.exists(fcc_file))
+      fcc_data <- get(load(fcc_file))
+      
+      stopifnot(meanCorr_dt$region %in% names(fcc_data))
+      
+      fcc_dt <- data.frame(region=names(fcc_data), fcc = as.numeric(fcc_data), stringsAsFactors = FALSE)
+      
+      out_dt <- merge(meanCorr_dt, fcc_dt, by=c("region"), all.x=TRUE, all.y=FALSE)
+      stopifnot(!is.na(out_dt))
+      
+      out_dt$hicds <- hicds
+      out_dt$exprds <- exprds
+      out_dt
+      
+    }
+    hicds_dt
+  }
+  
+  outFile <- file.path(outFolder, paste0("all_fcc_corr_dt.Rdata"))
+  save(all_fcc_corr_dt, file = outFile, version=2)
+  cat(paste0("... written: ", outFile, "\n"))
+} else {
+  inFile <- file.path(outFolder, paste0("all_fcc_corr_dt.Rdata"))
+  all_dt <- get(load(inFile))
+  # load("SAMPLEMEANCORR_FCC/all_fcc_corr_dt.Rdata")
+}
+
+densplot(
+y=  all_fcc_corr_dt$sample_meanCorr,
+x=  all_fcc_corr_dt$fcc
+)
+
+
+plot(
+  y=  all_fcc_corr_dt$sample_meanCorr[all_fcc_corr_dt$fcc>0.75],
+  x=  all_fcc_corr_dt$fcc[all_fcc_corr_dt$fcc>0.75]
+)
+
+plot(
+  y=  all_fcc_corr_dt$sample_meanCorr[all_fcc_corr_dt$hicds==hicds & all_fcc_corr_dt$exprds == exprds],
+  x=  all_fcc_corr_dt$fcc[all_fcc_corr_dt$hicds==hicds & all_fcc_corr_dt$exprds == exprds]
+)
+  
