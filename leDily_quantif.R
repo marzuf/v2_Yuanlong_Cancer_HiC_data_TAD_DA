c6997e402d296a3da9e0278253e7b7ca049e29c0
mzuer <z_marie_7@hotmail.com>
2020-08-07 18:33:30 +0200

    7.8.20_1

--- leDily_quantif.R
+++ leDily_quantif.R
@@ -2,6 +2,11 @@
 
 # Rscript leDily_quantif.R
 
+source("../Cancer_HiC_data_TAD_DA/utils_fct.R")
+require(reshape2)
+
+plotCex <- 1.2
+
 outFolder <- file.path("LEDILY_QUANTIF")
 dir.create(outFolder, recursive = TRUE)
 
@@ -40,9 +45,20 @@ gene_tad_dt$geneDElab <- ifelse(gene_tad_dt$adj.P.Val > geneSignifThresh, "notDE
 stopifnot(!is.na(gene_tad_dt$geneDElab))
 
 stopifnot(! (final_dt_withRank$meanLogFC_downRank <= 100 & final_dt_withRank$meanLogFC_upRank <= 100))
-final_dt_withRank$concordanceLab <- ifelse(final_dt_withRank$ratioDown >= thresh1 | final_dt_withRank$ratioDown <= thresh2, "concordant_TAD", "discordant_TAD")
+final_dt_withRank$concordanceLab <- ifelse(final_dt_withRank$ratioDown >= thresh1 | final_dt_withRank$ratioDown <= thresh2, 
+                                           "concordant_TAD", "discordant_TAD")
+
+final_dt_withRank$concordanceDirLab <- ifelse(final_dt_withRank$ratioDown >= thresh1,  "concordantDown_TAD",
+                                              ifelse(final_dt_withRank$ratioDown <= thresh2,  "concordantUp_TAD", "discordant_TAD"))
+stopifnot(
+  which(final_dt_withRank$concordanceDirLab == "discordant_TAD") == which(final_dt_withRank$concordanceLab == "discordant_TAD")
+)
+
+
 final_dt_withRank$activationLab <- ifelse(final_dt_withRank$meanLogFC_upRank <= 100, "activated_TAD", 
                                           ifelse(final_dt_withRank$meanLogFC_downRank <= 100, "repressed_TAD", "nonresp_TAD")) 
+# final_dt_withRank$dirLab <- ifelse(final_dt_withRank$meanLogFC > 0, "up", "down")
+# stopifnot(!is.na(final_dt_withRank$dirLab))
 
 
 final_dt_withRank$tadDAlab <- ifelse(final_dt_withRank$adjPvalComb > tadSignifThresh, "notDA_TAD",
@@ -52,7 +68,7 @@ stopifnot(!is.na(final_dt_withRank$tadDAlab))
 
 
 all_merged_dt <- merge(gene_tad_dt[,c("hicds", "exprds", "region", "entrezID", "geneDElab")], 
-                       final_dt_withRank[,c("hicds", "exprds", "region", "activationLab", "concordanceLab", "tadDAlab")], 
+                       final_dt_withRank[,c("hicds", "exprds", "region", "activationLab", "concordanceLab", "tadDAlab", "concordanceDirLab")], 
                        by=c("hicds", "exprds", "region"), all = TRUE)
 stopifnot(!is.na(all_merged_dt))
 
@@ -60,6 +76,7 @@ stopifnot(!is.na(all_merged_dt))
 all_merged_dt$dataset <- file.path(all_merged_dt$hicds, all_merged_dt$exprds)
 
 x=all_merged_dt[all_merged_dt$dataset==all_merged_dt$dataset[1],]
+save(x, file="x.Rdata", version=2)
 
 all_stats_dt <- do.call(rbind, by(all_merged_dt, all_merged_dt$dataset, function(x) {
   
@@ -80,10 +97,30 @@ all_stats_dt <- do.call(rbind, by(all_merged_dt, all_merged_dt$dataset, function
   
   # B)
   # % of signif up/down DE genes in activated/repressed TADs
+  # => % of upDE_and_activated | downDE_and_repressed
   upDE_and_activated <- mean(x$geneDElab == "upDE_gene" & x$activationLab == "activated_TAD")
   upDE_and_upDA <- mean(x$geneDElab == "upDE_gene" & x$tadDAlab == "upDA_TAD")
   downDE_and_repressed <- mean(x$geneDElab == "downDE_gene" & x$activationLab == "repressed_TAD")
   downDE_and_downDA <- mean(x$geneDElab == "downDE_gene" & x$tadDAlab == "downDA_TAD")
+  upDEdownDE_and_activrepr <- mean(
+    (x$geneDElab == "upDE_gene" & x$activationLab == "activated_TAD") |
+      (x$geneDElab == "downDE_gene" & x$activationLab == "repressed_TAD"))
+  upDEdownDE_and_upDAdownDA <- mean(
+    (x$geneDElab == "upDE_gene" & x$tadDAlab == "upDA_TAD") |
+      (x$geneDElab == "downDE_gene" & x$tadDAlab == "downDA_TAD"))
+  if(upDEdownDE_and_upDAdownDA != upDE_and_upDA+downDE_and_downDA) {
+    cat("ds=",ds,"\n")
+    cat("upDEdownDE_and_upDAdownDA=\t", upDEdownDE_and_upDAdownDA, "\n")
+    cat("upDE_and_upDA=\t", upDE_and_upDA, "\n")
+    cat("downDE_and_downDA=\t", downDE_and_downDA, "\n")  
+  }
+  
+  # cat("upDEdownDE_and_activrepr=\t", upDEdownDE_and_activrepr, "\n")
+  # cat("upDE_and_activated=\t", upDEdownDE_and_upDAdownDA, "\n")
+  # cat("downDE_and_repressed=\t", downDE_and_repressed, "\n")
+  
+  stopifnot(abs(upDEdownDE_and_upDAdownDA- (upDE_and_upDA+downDE_and_downDA)) <= 10^-4)
+  stopifnot(abs(upDEdownDE_and_activrepr - (upDE_and_activated+downDE_and_repressed)) <= 10^-4)
   
   # % of signif in nonresp TADs
   signifDE_and_notResp <- mean(x$geneDElab != "notDE_gene" & x$activationLab == "nonresp_TAD")
@@ -92,11 +129,39 @@ all_stats_dt <- do.call(rbind, by(all_merged_dt, all_merged_dt$dataset, function
   # C)
   # % of TADs with ratioDown >= 75% and <= 25% ("concordant_TAD")
   # % of DE genes in concordant
-  x_t <- x[,c("dataset", "region", "concordanceLab")]
+  x_t <- x[,c("dataset", "region", "concordanceLab", "activationLab", "concordanceDirLab")]
   x_t <- unique(x_t)
   concordantTAD <- mean(x_t$concordanceLab == "concordant_TAD")
   signifDE_and_concord <- mean(x$geneDElab != "notDE_gene" & x$concordanceLab == "concordant_TAD")
   all_and_concord <- mean(x$concordanceLab == "concordant_TAD")
+  
+  # Among TAD genes:
+  #   => ratio upDE in activated
+  # => ratio downDE in repressed
+  # => ratio upDE in nonresp.
+  # => ratio downDE in nonresp.
+  
+  ratioGenes_inTAD_dt <- do.call(rbind, by(x, x$region, function(tad) {
+    tadlab <- unique(tad$activationLab)
+    stopifnot(length(tadlab) == 1)
+    ratioUpDE <- mean(tad$geneDElab == "upDE_gene")
+     ratioDownDE <- mean(tad$geneDElab == "downDE_gene")
+    data.frame(tadlab=tadlab,
+               ratioUpDE=ratioUpDE,
+               ratioDownDE=ratioDownDE,
+               stringsAsFactors = FALSE)
+  }))
+  upDE_in_activatedTAD <- mean(ratioGenes_inTAD_dt$ratioUpDE[ratioGenes_inTAD_dt$tadlab=="activated_TAD"])
+  downDE_in_repressedTAD <- mean(ratioGenes_inTAD_dt$ratioDownDE[ratioGenes_inTAD_dt$tadlab=="repressed_TAD"])
+  upDE_in_nonrespTAD <- mean(ratioGenes_inTAD_dt$ratioUpDE[ratioGenes_inTAD_dt$tadlab=="nonresp_TAD"])
+  downDE_in_nonrespTAD <- mean(ratioGenes_inTAD_dt$ratioDownDE[ratioGenes_inTAD_dt$tadlab=="nonresp_TAD"])
+  
+  
+  # Among all TADs:
+  #   => % with ratioDown >= 0.75 in repressed and <= 0.25 in activated
+ concordantDown_and_repressed <- mean(x_t$concordanceDirLab == "concordantDown_TAD" & x_t$activationLab == "repressed_TAD")
+ concordantUp_and_activated <- mean(x_t$concordanceDirLab == "concordantUp_TAD" & x_t$activationLab == "activated_TAD")
+  
   
   data.frame(
     dataset = ds,
@@ -108,11 +173,19 @@ all_stats_dt <- do.call(rbind, by(all_merged_dt, all_merged_dt$dataset, function
     upDE_and_upDA=upDE_and_upDA,
     downDE_and_repressed=downDE_and_repressed,
     downDE_and_downDA=downDE_and_downDA,
+    upDEdownDE_and_activrepr=upDEdownDE_and_activrepr,
+    upDEdownDE_and_upDAdownDA=upDEdownDE_and_upDAdownDA,
     signifDE_and_notResp=signifDE_and_notResp,
     signifDE_and_notDA=signifDE_and_notDA,
     concordantTAD=concordantTAD,
     signifDE_and_concord=signifDE_and_concord,
     all_and_concord=all_and_concord,
+    concordantDown_and_repressed=concordantDown_and_repressed,
+    concordantUp_and_activated=concordantUp_and_activated,
+    upDE_in_activatedTAD=upDE_in_activatedTAD,
+    downDE_in_repressedTAD=downDE_in_repressedTAD,
+    upDE_in_nonrespTAD=upDE_in_nonrespTAD,
+    downDE_in_nonrespTAD=downDE_in_nonrespTAD,
     stringsAsFactors=FALSE
   )
   
@@ -128,7 +201,34 @@ save(all_stats_dt, file=outFile,version=2)
 cat(paste0("... written: ", outFile,"\n"))
 
 
-source("../Cancer_HiC_data_TAD_DA/utils_fct.R")
+# boxplot(NUMS ~ GRP, data = ddf, lwd = 2, ylab = 'NUMS')
+# stripchart(NUMS ~ GRP, vertical = TRUE, data = ddf, 
+#            method = "jitter", add = TRUE, pch = 20, col = 'blue')
+
+plot_dt <- all_stats_dt[,c("dataset", "signifDE_and_resp", "all_and_resp")]
+plot_dt_m <- melt(plot_dt,id="dataset")
+par(bty="L")
+boxplot(value ~ variable, data = plot_dt_m, lwd = 2,
+        cex.main=plotCex, cex.lab=plotCex,cex.axis=plotCex,
+        xlab="", ylab = 'average ratio of genes')
+stripchart(value ~ variable, vertical = TRUE, data = plot_dt_m, 
+            method = "jitter", add = TRUE, pch = 20, col = 'blue')
+
+
+plot_density_2_vars <- function(var1, var2, data, saveF=TRUE, ...) {
+  stopifnot(var1 %in% colnames(data))
+  stopifnot(var2 %in% colnames(data))
+  plot_list <- list(data[,var1],data[,var2])
+  names(plot_list) <- c(var1, var2)
+  if(saveF) outFile <- file.path(outFolder, paste0(var1,"_AND_", var2, ".png"))
+  if(saveF) png(outFile, height=350, width=550)
+  plot_multiDens(plot_list, ...)
+  if(saveF) foo <- dev.off()
+}
+
+plot_density_2_vars("signifDE_and_resp", "all_and_resp",all_stats_dt, my_xlab= "ratio of genes", legPos = "topleft")
+
+stop("-ok\n")
 
 outFile <- file.path(outFolder, "signifDE_and_resp_AND_all_and_resp.png")
 png(outFile, height=350, width=550)
@@ -199,6 +299,28 @@ plot_multiDens(
 )
 foo <- dev.off()
 
+outFile <- file.path(outFolder, "concordance_and_activation.png")
+png(outFile, height=350, width=550)
+plot_multiDens(
+  list(
+    concordantDown_and_repressed = all_stats_dt$concordantDown_and_repressed,
+    concordantUp_and_activated=all_stats_dt$concordantUp_and_activated
+  ), legPos = "topright", my_xlab="ratio of TADs"
+)
+foo <- dev.off()
+
+
+outFile <- file.path(outFolder, "geneDE_within_TADs.png")
+png(outFile, height=350, width=550)
+plot_multiDens(
+  list(
+    upDE_in_activatedTAD=all_stats_dt$upDE_in_activatedTAD,
+    downDE_in_repressedTAD=all_stats_dt$downDE_in_repressedTAD,
+    upDE_in_nonrespTAD=all_stats_dt$upDE_in_nonrespTAD,
+    downDE_in_nonrespTAD=all_stats_dt$downDE_in_nonrespTAD
+  ), legPos = "topright", my_xlab="ratio of genes in TAD"
+)
+foo <- dev.off()
 
 # 
 # 
---
+++ smilePlot_random.R
@@ -0,0 +1,229 @@
+#!/usr/bin/Rscript
+
+# Rscript smilePlot_random.R
+
+script_name <- "smilePlot_random.R"
+
+options(scipen=100)
+
+startTime <- Sys.time()
+
+suppressPackageStartupMessages(library(Hmisc, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE)) # error bar
+require(foreach)
+require(doMC)
+registerDoMC(40)
+
+
+# set files and folders
+script8_name <- "8cOnlyRatioDownFastSave_runAllDown"
+pipFolder <- file.path("PIPELINE", "OUTPUT_FOLDER")
+settingFolder <- file.path("PIPELINE", "INPUT_FILES")
+mainFolder <- "."
+setDir <-""
+
+# settings for plotting
+thresh1 <- 0.75
+thresh2 <- 1-thresh1
+nRandomInit <- 100000
+keepPermut <- 1000 # because converted as vector -> too big if keep 100000
+nRandom <- keepPermut
+curr_ratio_type <- "ratioDown"
+
+## TO SET
+## hard-coded:
+obsCol <- "bisque"
+obsColText <- "bisque2"
+permutCol <- "mediumseagreen"
+plotType <- "svg"
+myHeight <- ifelse(plotType == "png", 1028 , 15)
+myWidth <- ifelse(plotType == "png", 686, 10)
+
+histBreakStep <- 0.1
+stopifnot( (1/histBreakStep) %% 1 == 0 )
+#***********************************************************************************
+
+all_patterns <- c("RANDOMMIDPOSDISC", "RANDOMMIDPOSSTRICT", "RANDOMMIDPOS") # have deleted the permdt for randommidpos...
+all_patterns <- c("RANDOMMIDPOSDISC", "RANDOMMIDPOSSTRICT") # have deleted the permdt for randommidpos...
+
+for(rd_patt in all_patterns) {
+
+  outFolder <- file.path("SMILEPLOT_RANDOM", rd_patt)
+  dir.create(outFolder, recursive=TRUE)
+  
+  
+  stopifnot(dir.exists(pipFolder))
+  all_hicds <- list.files(pipFolder)
+  all_hicds <- all_hicds[grepl(paste0(rd_patt, "_40kb"),  all_hicds)]
+  stopifnot(dir.exists(file.path(mainFolder, all_hicds)))
+  all_exprds <- lapply(all_hicds, function(x) list.files(file.path(pipFolder, x)))
+  names(all_exprds) <- all_hicds
+  
+  all_missing_data <- foreach(hicds = all_hicds) %dopar% {
+    missing_data <- foreach(exprds = all_exprds[[paste0(hicds)]]) %do% {
+      
+      
+      outFile <- file.path(outFolder, paste0(hicds, "_", exprds, "_smilePlot.", plotType))
+      if(file.exists(outFile)) return(NULL)
+      
+      pipOutFold <- file.path(pipFolder, hicds, exprds)
+      
+      setDir <- ""
+      
+      cat(paste0("setDir =", setDir, "\n"))
+      
+      settingF <- file.path(settingFolder,hicds, paste0("run_settings_", exprds, ".R"))
+      source(settingF)
+      
+      curr_ratio_breaks <- seq(0,1,histBreakStep)
+      
+      cat(paste0("*** START smile plot for: ", hicds, " - " ,exprds, " - ", curr_ratio_type, "\n"))
+      
+      cat("... load observed data\n")
+      
+      obs_curr_down <- eval(parse(text = load(paste0(pipOutFold, "/", script8_name, "/all_obs_", curr_ratio_type, ".Rdata"))))
+      
+      cat("... load permut data\n")
+      permut_currDown_DT <- eval(parse(text = load(paste0(pipOutFold, "/", script8_name, "/", curr_ratio_type, "_permDT.Rdata"))))
+      
+      cat(ncol(permut_currDown_DT), "\n")
+      
+      if(ncol(permut_currDown_DT) != nRandomInit) return(paste0(hicds, "-", exprds))
+      
+      stopifnot(ncol(permut_currDown_DT) == nRandomInit)
+      
+      permut_currDown_DT <- permut_currDown_DT[,1:keepPermut]
+      stopifnot(!is.na(permut_currDown_DT))
+      stopifnot(grepl("_TAD",  rownames(permut_currDown_DT)))
+      
+      obsThresh <- mean(obs_curr_down >= thresh1 | obs_curr_down <= thresh2)
+      stopifnot( obsThresh >= 0 & obsThresh <= 1)
+      allThreshPermut <- unlist(apply(permut_currDown_DT, 2, function(x) mean(x >= thresh1 | x <= thresh2)))
+      stopifnot( allThreshPermut >= 0 & allThreshPermut <= 1)
+      stopifnot(length(allThreshPermut) == keepPermut)
+      
+      cat("> Prepare simulation data \n")
+      nTAD <- sum(regexpr("_TAD",  rownames(permut_currDown_DT)) > 0)
+      stopifnot(nTAD == nrow(permut_currDown_DT))
+      cat(paste0("... TADs: ", nTAD, "/", nrow(permut_currDown_DT), "\n"))
+      permut_currDown_vect <- as.vector(permut_currDown_DT[grep("_TAD", rownames(permut_currDown_DT)),])
+      stopifnot(length(permut_currDown_vect) == ncol(permut_currDown_DT) * nTAD)
+      
+      cat(paste0("length(permut_currDown_vect) = ", length(permut_currDown_vect), "\n"))
+      
+      cat("> Prepare observed data \n")
+      nTAD <- sum(regexpr("_TAD",  names(obs_curr_down)) > 0)
+      cat(paste0("... TADs: ", nTAD, "/", length(obs_curr_down), "\n"))
+      obs_curr_down <- obs_curr_down[grep("_TAD", names(obs_curr_down))]
+      
+      cat("> Prepare data for plotting \n")
+      
+      obs_rel_hist <- try(hist(obs_curr_down, breaks=curr_ratio_breaks, plot=F))
+      #plot(obs_rel_hist)
+      # some are not bounded 0-1 => go to next directly !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"WILL NEED REFINEMENT!!!
+      if(class(obs_rel_hist) == "try-error") {
+        warning(paste0("!!! CANNOT PLOT HISTOGRAM FOR: ", curr_ratio_type, " !!! \n" ))
+        next
+      }
+      cat(paste0("length(permut_currDown_vect) = ", length(permut_currDown_vect), "\n"))
+      cat(paste0("length(permut_currDown_vect) = ", length(na.omit(permut_currDown_vect)), "\n"))
+      shuff_rel_hist <-  hist(permut_currDown_vect,breaks=curr_ratio_breaks, plot=F)
+      # plot(shuff_rel_hist)
+      
+      cat(paste0("ncol(permut_currDown_DT) = ", ncol(permut_currDown_DT), "\n"))
+      cat(paste0("sum(shuff_rel_hist$counts) = ", sum(shuff_rel_hist$counts), "\n"))
+      cat(paste0("sum(shuff_rel_hist$counts/nRandom) = ", sum(shuff_rel_hist$counts/nRandom), "\n"))
+      cat(paste0("sum(obs_rel_hist$counts) = ", sum(obs_rel_hist$counts),"\n"))
+      stopifnot(sum(shuff_rel_hist$counts/nRandom) == sum(obs_rel_hist$counts))   # 830
+      stopifnot(sum(shuff_rel_hist$counts) == length(permut_currDown_vect))
+      stopifnot(sum(obs_rel_hist$counts) == length(na.omit(obs_curr_down)))
+      
+      rel_freqValues <- rbind(obs_rel_hist$counts, shuff_rel_hist$counts/nRandom)
+      rownames(rel_freqValues) <- c("observed", "randomized")
+      
+      # FOR THE ERROR BARS:
+      # for each randomization, calculate the observed, then divide expected by observed
+      # table 1 column = 1 randomization, rows = breaks of the hist.
+      rel_HistValues <- apply(permut_currDown_DT[grep("_TAD", rownames(permut_currDown_DT)),],
+                              2, function(x) hist(x,breaks=curr_ratio_breaks, plot=F)$counts)
+      stopifnot(nrow(rel_HistValues) == length(obs_rel_hist$counts))
+      # for each randomization, divide observ./exp.
+      rel_obsOverExp <- apply(rel_HistValues, 2, function(x) log2(obs_rel_hist$counts/x))
+      # now get the SEM for each break of the hist (i.e. each row)
+      rel_obsOverExp_sem <- apply(rel_obsOverExp, 1, function(x) {
+        x <- na.omit(x[is.finite(x)])
+        sd(x, na.rm=T)/sqrt(length(x))
+      })
+      
+      # Calculate observed/expected ratio
+      rel_logRatio <- log2(rel_freqValues["observed",]/rel_freqValues["randomized",])
+      # put y coord at the middle of the breaks
+      rel_ycoord <- (curr_ratio_breaks * 100)[-1]
+      rel_ycoord <- rel_ycoord-(histBreakStep*100)/2
+      stopifnot(length(rel_ycoord) == length(rel_logRatio))
+      toKeep <- !is.na(rel_logRatio) & abs(rel_logRatio) != "Inf"
+      rel_logRatio <- rel_logRatio[toKeep]
+      rel_ycoord <- rel_ycoord[toKeep]
+      rel_obsOverExp_sem <- rel_obsOverExp_sem[toKeep] 
+      my_ylab <- paste0(rel_ycoord - (histBreakStep*100)/2, "-", rel_ycoord + (histBreakStep*100)/2, "%")
+      plotTit <- paste0(hicds, " - ", cond1, " vs. ", cond2)
+      subtitDir1 <- paste0("kept # permut=", keepPermut, "; >=",  thresh1, "|<=", thresh2, ": ", round(obsThresh*100, 2),
+                           "% (avg. permut: ", round(mean(allThreshPermut) * 100, 2), "%)")
+      subtitDir2 <- ""
+      rel_ycoord <- rel_ycoord/100
+      
+      ####### PLOT HERE
+      outFile <- file.path(outFolder, paste0(hicds, "_", exprds, "_smilePlot.", plotType))
+      do.call(plotType, list(outFile, height=myHeight, width=myWidth))
+      par(mfrow = c(2,1))
+      # top panel: line plot
+      myxlab <-  paste0("% of down-regulated genes per TAD")
+      
+      # A) TOP PLOT - smile plot
+      plot(rel_logRatio ~ rel_ycoord, type="l",axes=F, cex.main=0.9,
+           main = plotTit,
+           xlab = myxlab, ylab = "log2(Observed/Randomized)")
+      mtext(side=3, paste0(subtitDir1))
+      box(bty="l")
+      axis(2)
+      axis(1, at=rel_ycoord, labels = my_ylab)
+      abline(h=0, lty=2)
+      errbar(x=rel_ycoord, y=rel_logRatio, yplus=rel_logRatio+rel_obsOverExp_sem, yminus=rel_logRatio-rel_obsOverExp_sem, add=T)
+      
+      # B) BOTTOM PLOT - histogram bar plot
+      barplot(rel_freqValues, beside=T, col=c(obsCol, permutCol), ylab="Frequency", 
+              xlab=myxlab)
+      legend("topright", c( "observed Freq.", "randomized Freq."),
+             text.col=c(obsColText, permutCol), bty='n', cex=1)
+      mtext(side=3, text = subtitDir2)
+      
+      rel_ycoord <- (curr_ratio_breaks * 100)[-1]
+      rel_ycoord <- rel_ycoord-(histBreakStep*100)/2
+      my_ylab <- rel_ycoord
+      cat("my_ylab\n", my_ylab, "\n")
+      xlabpos <-  seq(2, 1/histBreakStep * 6, by=3)
+      cat("xlabpos\n", xlabpos, "\n")
+      box(bty="l")
+      axis(1)
+      axis(2, at=c(0, max(rel_freqValues, na.rm=T)), labels=F)
+      foo <- dev.off()
+      cat(paste0("... written: ", outFile, "\n"))
+      return(NULL)
+    }
+    missing_data
+  }
+  outFile <- file.path(outFolder, "all_missing_data.Rdata")
+  save(all_missing_data, file=outFile, version=2)
+  
+  
+}
+
+
+
+###################################################################
+txt <- paste0(startTime, "\n", Sys.time(), "\n")
+cat(txt)
+cat(paste0("*** DONE: ", script_name, "\n"))
+
+
+
+
