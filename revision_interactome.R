7cfda9d85ad72c79b54127e0186ab09d86844bdb
mzuer <z_marie_7@hotmail.com>
2021-03-05 12:24:03 +0100

    5.3_1

--- revision_interactome.R
+++ revision_interactome.R
@@ -1,38 +1,163 @@
-dt=get(load("hic_dc_comp_exp_info.Rdata"))
 
-source("revision_settings.R")
+# Rscript revision_interactome.R
+
+require(foreach)
+require(doMC)
+registerDoMC(50)
+
+initbinsize <- 40000
+hicdcbinsize <- 20000
+signifThresh <- 0.05
+
+buildTable <- TRUE
+
+setDir <- "/media/electron"
+setDir <- ""
+
+all_pairs_cols <- c("ENCSR346DCU_LNCaP_40kb" = "LNCaP",
+                    "GSE118514_RWPE1_40kb" = "RWPE1",
+                    "GSE118514_22Rv1_40kb"="22Rv1")
 
 all_chrs <- paste0("chr", 1:22)
 
-all_interactome_dt <- do.call(rbind, lapply(seq_along(dt), function(i) {
+ds=names(all_pairs_cols)[1]
+
+if(buildTable){
+  # all_hicdc_dt <- foreach(ds = c("GSE118514_22Rv1_40kb", .combine='rbind') %do% {
   
-  i_dt <- dt[[i]]
+  all_hicdc_dt <- foreach(ds = names(all_pairs_cols), .combine='rbind') %do% {
+    
+    cat(paste0("start ", ds, "\n"))
+    
+    dt <- get(load(file.path(setDir,
+                             "/mnt/ndata/Yuanlong/1.Projects/19.With_Marie/1.Data",
+                             paste0(ds, "_TADs_for_hicdc.output.Rdata"))))
+    # level 1 - list of TADs,
+    # level 2 - for a given TAD
+    # - 2a "hic_dc_mat"  = list of 3 matrices ("LNCaP" "22Rv1" "RWPE1")
+    # - 2b"summary_tab" = data frame with colnames:
+    # [1] "gene"                          "LNCaP_prostate_cancer"        
+    # [3] "22Rv1_prostate_cancer"         "RWPE1_prostate_epithelium"    
+    # [5] "exp_LNCaP_prostate_cancer"     "exp_22Rv1_prostate_cancer"    
+    # [7] "exp_RWPE1_prostate_epithelium" "sig_ratio_LNCaP"              
+    # [9] "sig_ratio_22Rv1"               "sig_ratio_RWPE1"              
+    # [11] "mean_p_LNCaP"                  "mean_p_22Rv1"                 
+    # [13] "mean_p_RWPE1"                  "pos_start"                    
+    # [15] "pos_end"                       "chr_num"                      
+    
+    # iterate over the TADs
+    all_interactome_dt <- foreach(i= seq_along(dt), .combine='rbind') %dopar% {
+      
+      i_dt <- dt[[i]]
+      summary_dt <- i_dt[["summary_tab"]]
+      
+      chromo <- gsub("(.+):.+:.+", "chr\\1", names(dt)[i])
+      stopifnot(chromo %in% all_chrs)
+      chromo_nbr <- as.numeric(gsub("(.+):.+:.+", "\\1", names(dt)[i]))
+      stopifnot(!is.na(chromo_nbr))
+      
+      tad_start <- as.numeric(gsub(".+:(.+):.+", "\\1", names(dt)[i]))
+      stopifnot(!is.na(tad_start))
+      tad_end <- as.numeric(gsub(".+:.+:(.+)", "\\1", names(dt)[i]))
+      stopifnot(!is.na(tad_end))
+      
+      
+      # stopifnot(tad_start == min(summary_dt[["pos_start"]]))# not always true, si e.g.
+      # > dt=get(load("/mnt/ndata/Yuanlong/1.Projects/19.With_Marie/1.Data/GSE118514_22Rv1_40kb_TADs_for_hicdc.output.Rdata"))                                                                              
+      # > names(dt)[9649]
+      # [1] "20:29400001:29840000"
+      # > min(dt[[9649]][["summary_tab"]][["pos_start"]])
+      # [1] 29440001
+      stopifnot(tad_start <= min(summary_dt[["pos_start"]]))# not always true, si e.g.
+      stopifnot(tad_end >= max(summary_dt[["pos_end"]]))
+      # stopifnot(tad_end == max(summary_dt[["pos_end"]]))
+      stopifnot(chromo_nbr == summary_dt[["chr_num"]])
+      
+      sub_dt <- unique(summary_dt[,grepl("mean_p", colnames(summary_dt)) | grepl("sig_ratio", colnames(summary_dt))])
+      if(nrow(sub_dt) != 1) {
+        outFile <- file.path(paste0(ds, "_",i, "_", "i_dt_debug.Rdata"))
+        save(i_dt, file=outFile, version=2)
+        outFile <- file.path(paste0(ds, "_",i, "_", "sub_dt_debug.Rdata"))
+        save(sub_dt, file=outFile, version=2)
+      }
+      # stopifnot(nrow(sub_dt) == 1)   # can be 0 e.g.                                     
+      # > dt[[5122]][["summary_tab"]]
+      # [1] gene                          LNCaP_prostate_cancer        
+      # [3] 22Rv1_prostate_cancer         RWPE1_prostate_epithelium    
+      # [5] exp_LNCaP_prostate_cancer     exp_22Rv1_prostate_cancer    
+      # [7] exp_RWPE1_prostate_epithelium sig_ratio_LNCaP              
+      # [9] sig_ratio_22Rv1               sig_ratio_RWPE1              
+      # [11] mean_p_LNCaP                  mean_p_22Rv1                 
+      # [13] mean_p_RWPE1                  pos_start                    
+      # [15] pos_end                       chr_num                      
+      # 
+      if(nrow(sub_dt) == 0) {
+        sub_dt[1,] <- NA
+      }
+      # return(NULL)
+      stopifnot(nrow(sub_dt) == 1)   # 
+      sub_dt$chromo <- chromo
+      sub_dt$tad_start <- tad_start
+      sub_dt$tad_end <- tad_end
+
+      if(ds== "GSE118514_22Rv1_40kb" & i == 5710) {
+        outFile <- file.path(paste0(ds, "_",i, "_", "i_dt_debug.Rdata"))
+        save(i_dt, file=outFile, version=2)
+        outFile <- file.path(paste0(ds, "_",i, "_", "sub_dt_debug.Rdata"))
+        save(sub_dt, file=outFile, version=2)
+      }
+      
+      
+      # sur chacune des submats, avec hicds comme ref. compter le nombre d'interactions qui sont différentes
+      all_mats <- i_dt[["hic_dc_mat"]]
+      ref_ds <- as.character(all_pairs_cols[ds])
+      i_ref <- which(names(all_mats) == ref_ds)
+      stopifnot(length(i_ref) == 1)
+      i_others <- which(names(all_mats) != ref_ds)
+      stopifnot(length(i_others) == 2)
+      ref_mat <- as.matrix(all_mats[[i_ref]])
+      stopifnot(nrow(ref_mat) == ncol(ref_mat))
+      
+      stopifnot(nrow(ref_mat) == (tad_end-tad_start+1)/initbinsize*(initbinsize/hicdcbinsize))
+      ref_values <- ref_mat[lower.tri(ref_mat, diag=TRUE)]
+      stopifnot(length(ref_values) == nrow(ref_mat) * (nrow(ref_mat)+1) * 0.5) 
+      i_mat=2
+      for(i_mat in i_others) {
+        match_mat <- all_mats[[i_mat]]
+        stopifnot(dim(match_mat) == dim(ref_mat))
+        match_values <- match_mat[lower.tri(match_mat, diag=TRUE)]
+        stopifnot(length(match_values) == nrow(match_mat) * (nrow(match_mat)+1) * 0.5)
+        # matching_vals <- na.omit((ref_values <= signifThresh) == (match_values <= signifThresh))
+        ### THEY ARE ALREADY IN -LOG10 !!!
+        matching_vals <- na.omit((ref_values >= -log10(signifThresh)) == (match_values >= -log10(signifThresh)))
+
+        nMatchingEntries <- length(matching_vals)
+        nSameEntries <- sum(matching_vals)
+        stopifnot(nSameEntries/nMatchingEntries >= 0 &  nSameEntries/nMatchingEntries <= 1)
+        sub_dt[,paste0("nMatchingEntries_", ref_ds, "_", names(all_mats)[i_mat])] <- nMatchingEntries
+        sub_dt[,paste0("nSameEntries_", ref_ds, "_", names(all_mats)[i_mat])] <- nSameEntries
+        sub_dt[,paste0("nRatioMatchEntries_", ref_ds, "_", names(all_mats)[i_mat])] <- nSameEntries/nMatchingEntries
+      } # end iterating over hicdc matrices
+      sub_dt
+    } # end iterating over TAD
+    
+    all_interactome_dt$hicds_hicdc <- ds
+    all_interactome_dt
+  } # end iterating over ds
   
-  chromo <- gsub("(.+):.+:.+", "chr\\1", names(dt)[i])
-  stopifnot(chromo %in% all_chrs)
-  chromo_nbr <- as.numeric(gsub("(.+):.+:.+", "\\1", names(dt)[i]))
-  stopifnot(!is.na(chromo_nbr))
-  
-  tad_start <- as.numeric(gsub(".+:(.+):.+", "\\1", names(dt)[i]))
-  stopifnot(!is.na(tad_start))
-  tad_end <- as.numeric(gsub(".+:.+:(.+)", "\\1", names(dt)[i]))
-  stopifnot(!is.na(tad_end))
-  
-  stopifnot(tad_start == min(i_dt[["pos_start"]]))
-  stopifnot(tad_end == max(i_dt[["pos_end"]]))
-  stopifnot(chromo_nbr == i_dt[["chr_num"]])
-  
-  sub_dt <- unique(i_dt[,grepl("mean_p", colnames(i_dt)) | grepl("sig_ratio", colnames(i_dt))])
-  stopifnot(nrow(sub_dt) == 1)                                        
-  sub_dt$chromo <- chromo
-  sub_dt$tad_start <- tad_start
-  sub_dt$tad_end <- tad_end
-  sub_dt
-}))
-  
-all_pairs_cols <- c("ENCSR346DCU_LNCaP_40kb" = "LNCaP",
-                    "GSE118514_RWPE1_40kb" = "RWPE1",
-                    "GSE118514_22Rv1_40kb"="22RV1")
+  outFile <- file.path(outFolder, "all_hicdc_dt.Rdata")
+  save(all_hicdc_dt, file=outFile, version=2)
+  cat(paste0("... written: ", outFile, "\n"))
+} else {
+  outFile <- file.path(outFolder, "all_hicdc_dt.Rdata")
+  all_hicdc_dt <- get(load(outFile))
+}
+stop("--ok \n")
+
+
+source("revision_settings.R")
+
+
 
 myds <- all_pairs[grepl("prad", basename(all_pairs))]
 all_cmps <- c(myds,
